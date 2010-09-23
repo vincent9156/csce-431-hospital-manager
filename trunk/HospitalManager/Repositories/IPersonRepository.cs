3a889c01b68b4993b0d61c71e3b78126e5557e35
genericnumber <genericnumber@350620d6-2aca-f82a-c46e-8b943422fb9b>
2010-09-23 02:43:34 +0000

    Initial commit of VS solution for architecture

---
+++ trunk/HospitalManager/Repositories/IPersonRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HospitalManager.Models;
+
+namespace HospitalManager.Repositories
+{
+    interface IPersonRepository
+    {
+        /**
+         * Get a person from the database by name
+         */
+        Person GetPersonByName(string firstName, string lastName);
+    }
+}
