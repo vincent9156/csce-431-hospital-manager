11702c893f4ac5afdb5da0b12c3533bedac1f91e
csmith3332 <csmith3332@350620d6-2aca-f82a-c46e-8b943422fb9b>
2010-09-26 23:32:04 +0000

    Added a DB mdf file for use with the application. Modified models, controllers, and views to use this new database. Application now has a home page and users page. On the users page you can see all users in the system, as well as create/update records.

--- HospitalManager/Controllers/HomeController.cs
+++ HospitalManager/Controllers/HomeController.cs
@@ -3,36 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using HospitalManager.Models;
-using HospitalManager.Repositories;
-using HospitalManager.ViewModels;
 
 namespace HospitalManager.Controllers
 {
     public class HomeController : Controller
     {
-        /**
-         * Our default action for when the user visits /Home/
-         */
+        //
+        // GET: /Home/
+
         public ActionResult Index()
         {
-            // Pull the information for person "Robert Paulson" from the repository
-            var personRepository = new PersonRepository();
-            Person p = personRepository.GetPersonByName("Robert", "Paulson");
-
-            // Re-wrap the information for the person
-            // The view doesn't need the person's credit card information
-            var robert = new PersonViewModel()
-            {
-                Id = p.Id,
-                FirstName = p.FirstName,
-                LastName = p.LastName,
-                Message = p.Message
-            };
-
-            // Give the PersonViewModel to the View (in this case, /Views/Home/Index)
-            return View(robert);
+            return View();
         }
-
     }
 }
