27ca387707dc132e80493b4b8b8293f1097f1022
genericnumber <genericnumber@350620d6-2aca-f82a-c46e-8b943422fb9b>
2010-10-06 17:55:37 +0000

    Partial work on registration completed. Committing to allow collaboration.

---
+++ HospitalManager/Controllers/AuthenticationController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HospitalManager.Repositories;
+using HospitalManager.ViewModels;
+using HospitalManager.Models;
+
+namespace HospitalManager.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        /**
+         * Get an instance of the user repository
+         */
+        UserRepository userRepository;
+
+        public AuthenticationController()
+        {
+            userRepository = new UserRepository();
+        }
+
+        //
+        // GET: /Authentication/
+
+        public ActionResult Index()
+        {
+            return Redirect("/Authentication/Login/");
+        }
+
+        public ActionResult Register()
+        {
+            var types = new UserTypesViewModel
+            {
+                UserTypes = userRepository.GetUserTypes()
+            };
+            return View(types);
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserType type)
+        {
+            User newUser = null;
+            switch (type.TypeID)
+            {
+                case UserType.PatientTypeID:
+                    newUser = new Patient();
+                    break;
+                case UserType.DoctorTypeID:
+                    newUser = new Doctor();
+                    break;
+                case UserType.NurseTypeID:
+                    newUser = new Nurse();
+                    break;
+                case UserType.PharmacistTypeID:
+                    newUser = new Pharmacist();
+                    break;
+            }
+            return View(newUser);
+        }
+        /*
+        public ActionResult Register(User user)
+        {
+
+        }
+        */
+    }
+}
---
+++ HospitalManager/ViewModels/UserTypesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HospitalManager.Models;
+
+namespace HospitalManager.ViewModels
+{
+    public class UserTypesViewModel
+    {
+        public IEnumerable<UserType> UserTypes { get; set; }
+    }
+}
