c64b4c8e73c6d03717ef1d70e53817030d42a6c5
csmith3332 <csmith3332@350620d6-2aca-f82a-c46e-8b943422fb9b>
2010-10-10 19:58:16 +0000

    Added a scheduling model to models folder

---
+++ trunk/HospitalManager/Repositories/ScheduleRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HospitalManager.Models;
+
+namespace HospitalManager.Repositories
+{
+    public class ScheduleRepository
+    {
+        private SchedulesDataContext db = new SchedulesDataContext();
+
+
+    }
+}
