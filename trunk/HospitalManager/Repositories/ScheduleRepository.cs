1c9a13b95349fb26d911ddb10f7953726bd778fc
csmith3332 <csmith3332@350620d6-2aca-f82a-c46e-8b943422fb9b>
2010-10-10 21:07:40 +0000

    Added a Schedule Interface and Repository

--- trunk/HospitalManager/Repositories/ScheduleRepository.cs
+++ trunk/HospitalManager/Repositories/ScheduleRepository.cs
@@ -6,10 +6,22 @@ using HospitalManager.Models;
 
 namespace HospitalManager.Repositories
 {
-    public class ScheduleRepository
+    public class ScheduleRepository : IScheduleRepository
     {
-        private SchedulesDataContext db = new SchedulesDataContext();
+        private SchedulesDataContext _db;
 
+        public ScheduleRepository()
+        {
+            _db = new SchedulesDataContext();
+        }
 
+        //Return A List Of All Doctors
+        public IList<Doctors> ListAll()
+        {
+            var docs = from d in _db.Doctors
+                       select d;
+
+            return docs.ToList();
+        }
     }
 }
