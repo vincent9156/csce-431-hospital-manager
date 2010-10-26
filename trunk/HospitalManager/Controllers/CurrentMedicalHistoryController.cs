0021d814cbd92221b395ee955e5b6088310eaee0
phelpy333@gmail.com <phelpy333@gmail.com@350620d6-2aca-f82a-c46e-8b943422fb9b>
2010-10-26 18:46:43 +0000

    Forgot to include CurrentMedicalHistory files last time in my update. They are still a work in progress

---
+++ trunk/HospitalManager/Controllers/CurrentMedicalHistoryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HospitalManager.Repositories;
+using HospitalManager.Models;
+using System.Web.Routing;
+using HospitalManager.Libraries;
+using HospitalManager.ViewModels;
+using System.Text.RegularExpressions;
+
+namespace HospitalManager.Controllers
+{
+    public class CurrentMedicalHistoryController : Controller
+    {
+        CurrentMedicalHistoryRepository histRep = new CurrentMedicalHistoryRepository();
+        SessionRepository sessRep = new SessionRepository();
+
+        
+        
+
+        //
+        // GET: /CurrentMedicalHistory/
+        
+        public ActionResult Index()
+        {
+            IQueryable<CurrentMedicalHistory> history = histRep.GetCurrentMedicalHistoryByUser(sessRep.GetUser()); 
+            
+            return View();
+        }
+
+    }
+}
---
+++ trunk/HospitalManager/Views/CurrentMedicalHistory/Index.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<HospitalManager.ViewModels.CurrentMedicalHistoryViewModel>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+	Current Medical History
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>Current Medical History</h2>
+
+     <% if (Model == null)
+        { %>
+     <p> No Current Medical History Found</p>
+    <% } else { %>
+
+    <br />
+    <h3>Most Recent Visit Information</h3>
+    <p>
+        Day -
+        <%: Model.Day %>
+    </p>
+    <p>
+        Month -
+        <%: Model.Month %>
+    </p>
+    <p>
+        Year -
+        <%: Model.Year %>
+    </p>
+    
+    <br />
+
+
+</asp:Content>
