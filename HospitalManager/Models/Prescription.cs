201a7770ca449cc5466df61615a1aac8f92cdf73
genericnumber <genericnumber@350620d6-2aca-f82a-c46e-8b943422fb9b>
2010-11-01 18:17:07 +0000

    Added start to prescription and medication functionality.

---
+++ HospitalManager/Models/Prescription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalManager.Models
+{
+    public partial class Prescription
+    {
+        public decimal GetCost()
+        {
+            return Quantity * mgPerPill * Medication.PricePerMg;
+        }
+    }
+}
---
+++ HospitalManager/Repositories/PrescriptionRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HospitalManager.Models;
+
+namespace HospitalManager.Repositories
+{
+    public class PrescriptionRepository
+    {
+        private PrescriptionsDatabase prescriptionDb = new PrescriptionsDatabase();
+
+        public int AddPrescription(Prescription prescription)
+        {
+            return 0;
+        }
+
+        public IQueryable<Medication> GetAllMedications()
+        {
+            return from script in prescriptionDb.Medications
+                   orderby script.MedicationName ascending
+                   select script;
+        }
+
+        public bool MedicationExists(int medID)
+        {
+            return true;
+        }
+    }
+}
