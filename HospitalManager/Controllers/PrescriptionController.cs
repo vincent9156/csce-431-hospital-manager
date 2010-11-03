09a8ee18535a4a56c32cc84713ce3622226830ac
gmskindell <gmskindell@350620d6-2aca-f82a-c46e-8b943422fb9b>
2010-11-03 17:38:49 +0000

    Added PrescriptionController.cs and PrescriptionViewModel.cs

---
+++ HospitalManager/Controllers/PrescriptionController.cs
@@ -0,0 +1,38 @@
+﻿// Coded my michael risley !! WOOooT
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HospitalManager.Repositories;
+using HospitalManager.ViewModels;
+using AutoMapper;
+using HospitalManager.Models;
+
+namespace HospitalManager.Controllers
+{
+    public class PrescriptionController : Controller
+    {
+        SessionRepository session = new SessionRepository();
+        UserRepository user = new UserRepository();
+
+        public ActionResult WritePrescription(int id)
+        {
+            var vm = new PrescriptionViewModel
+            {
+                userId = id,
+            };
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult FillPrescription(PrescriptionViewModel vm)
+        {
+            vm.docId = session.GetUser().UserID;
+            Mapper.Map<PrescriptionViewModel, Prescription>(vm);
+            return View(vm);
+        }
+
+    }
+}
---
+++ HospitalManager/ViewModels/PrescriptionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalManager.ViewModels
+{
+    public class PrescriptionViewModel
+    {
+        public int userId;
+        public int docId;
+        public int quantity;
+        public int numRefills;
+        public int mgPerPill;
+        public string instruction;
+        public string medName;
+    }
+}
