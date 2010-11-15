67ea5eae4cf60ccf8c077a645984df37dfdf0ffb
genericnumber <genericnumber@350620d6-2aca-f82a-c46e-8b943422fb9b>
2010-11-15 18:33:21 +0000

    Added functionality to BillingInformationRepository.

--- HospitalManager/Repositories/BillingInformationRepository.cs
+++ HospitalManager/Repositories/BillingInformationRepository.cs
@@ -8,19 +8,44 @@ namespace HospitalManager.Repositories
 {
     public class BillingInformationRepository
     {
-        CreditCardInformation GetCreditCardInformation(int UserID)
+        UserBillingInformationDatabase infoDb = new UserBillingInformationDatabase();
+
+        /**
+         * Get the credit card information for a user
+         */
+        CreditCardInformation GetCreditCardInfo(int UserID)
         {
-            return null;
+            return infoDb.CreditCardInformations.Single(u => u.UserID == UserID);
         }
 
+        /**
+         * Edit a user's billing information
+         */
         void EditBillingInfo(CreditCardInformation info)
         {
+            var result = (from dbInfo in infoDb.CreditCardInformations
+                          where dbInfo.UserID == info.UserID
+                          select dbInfo).Single();
 
+            result.CardProviderID = info.CardProviderID;
+            result.CardNumber = info.CardNumber;
+            result.SecurityCode = info.SecurityCode;
+            result.ExpirationMonth = info.ExpirationMonth;
+            result.ExpirationYear = info.ExpirationYear;
+            result.BillingAddress = info.BillingAddress;
+            result.InsurancePolicyNumber = info.InsurancePolicyNumber;
+            result.InsuranceProviderName = info.InsuranceProviderName;
+
+            infoDb.SubmitChanges();
         }
 
+        /**
+         * Create a user's billing information
+         */
         void CreateBillingInfo(CreditCardInformation info)
         {
-
+            infoDb.CreditCardInformations.InsertOnSubmit(info);
+            infoDb.SubmitChanges();
         }
 
     }
