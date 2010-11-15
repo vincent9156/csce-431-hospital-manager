f004d538217756dd38ff0e4dde8c7acf177ff1c0
genericnumber <genericnumber@350620d6-2aca-f82a-c46e-8b943422fb9b>
2010-11-15 18:08:00 +0000

    Added billing information repositories and models.

---
+++ trunk/HospitalManager/Repositories/BillingInformationRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HospitalManager.Models;
+
+namespace HospitalManager.Repositories
+{
+    public class BillingInformationRepository
+    {
+        CreditCardInformation GetCreditCardInformation(int UserID)
+        {
+            return null;
+        }
+
+        void EditBillingInfo(CreditCardInformation info)
+        {
+
+        }
+
+        void CreateBillingInfo(CreditCardInformation info)
+        {
+
+        }
+
+    }
+}
