c754551752d56bc312626d2fc5237ee488fffb94
gmskindell <gmskindell@350620d6-2aca-f82a-c46e-8b943422fb9b>
2010-11-28 03:47:12 +0000

    Just some comments on the SearchViewModel

--- HospitalManager/ViewModels/SearchViewModel.cs
+++ HospitalManager/ViewModels/SearchViewModel.cs
@@ -6,9 +6,12 @@ using HospitalManager.Models;
 
 namespace HospitalManager.ViewModels
 {
+    /// <summary>
+    /// ViewModel used by SearchController to store results of searching for Users in the database
+    /// </summary>
     public class SearchViewModel
     {
-        public UserViewModel LoggedInUser;
-        public IList<UserViewModel> SearchResults;
+        public UserViewModel LoggedInUser; //used for checking permissions on seacrching
+        public IList<UserViewModel> SearchResults; 
     }
 }
