e560f369e3277ad6d0f3f55cae83fba10ad18624
mattbartel3@gmail.com <mattbartel3@gmail.com@350620d6-2aca-f82a-c46e-8b943422fb9b>
2010-11-29 17:39:53 +0000

    More comments

--- trunk/HospitalManager/ViewModels/UpdateMedicalHistoryViewModel.cs
+++ trunk/HospitalManager/ViewModels/UpdateMedicalHistoryViewModel.cs
@@ -6,10 +6,24 @@ using HospitalManager.Models;
 
 namespace HospitalManager.ViewModels
 {
+    /// <summary>
+    /// ViewModel used when updating the user's past medical history
+    /// </summary>
     public class UpdateMedicalHistoryViewModel
     {
+        /// <summary>
+        /// The user's past medical history
+        /// </summary>
         public PastMedicalHistory UserHistory { get; set; }
+
+        /// <summary>
+        /// List of medical conditions
+        /// </summary>
         public IList<MedicalCondition> AllMedicalConditions { get; set; }
+
+        /// <summary>
+        /// List of family members
+        /// </summary>
         public IList<FamilyMember> AllFamilyMembers { get; set; }
     }
 }
