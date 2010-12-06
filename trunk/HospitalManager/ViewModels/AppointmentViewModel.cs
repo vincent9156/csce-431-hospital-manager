e3a2727b22e3cc55da4a647c68960007d63e25eb
csmith3332 <csmith3332@350620d6-2aca-f82a-c46e-8b943422fb9b>
2010-12-06 06:02:42 +0000

    Added more comments to the appointment code

--- trunk/HospitalManager/ViewModels/AppointmentViewModel.cs
+++ trunk/HospitalManager/ViewModels/AppointmentViewModel.cs
@@ -14,20 +14,69 @@ namespace HospitalManager.ViewModels
     public class AppointmentViewModel
     {
 
+        /// <summary>
+        /// Used for deleting appointments
+        /// </summary>
         public int AppointmentID { get; set; }
+
+        /// <summary>
+        /// Used for deleting, adding, and displaying appointments
+        /// </summary>
         public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Used for deleting, adding appointments
+        /// </summary>
         public int UserID { get; set; }
+
+        /// <summary>
+        /// Used for deleting, adding appointments
+        /// </summary>
         public int DoctorID { get; set; }
-        //Displayed data
+
+        /// <summary>
+        /// Used for deleting, adding, and displaying appointments
+        /// </summary>
         public TimeSpan Time { get; set; }
+
+        /// <summary>
+        /// Used for displaying appointments
+        /// </summary>
         public string PatientFirstName { get; set; }
+
+        /// <summary>
+        /// Used for displaying appointments
+        /// </summary>
         public string PatientLastName { get; set; }
+
+        /// <summary>
+        /// Used for displaying appointments
+        /// </summary>
         public string DoctorFirstName { get; set; }
+
+        /// <summary>
+        /// Used for displaying appointments
+        /// </summary>
         public string DoctorLastName { get; set; }
 
+        /// <summary>
+        /// Used for displaying appointments
+        /// </summary>
         public IList<AppointmentViewModel> appointments;
+
+        /// <summary>
+        /// Used for displaying doctors when scheduling appointments
+        /// </summary>
         public List<User> Doctors { get; set; }
+
+        /// <summary>
+        /// Used for displaying times when scheduling appointments
+        /// </summary>
         public List<TimeSpan> Times { get; set; }
+
+        /// <summary>
+        /// Used for displaying messages to the user
+        /// </summary>
         public string Message { get; set; }
     }
 }
